4647fe792cd47abe6f044656cbfd153cf409c659
Terrence Miao <terrence.miao@gmail.com>
2024-03-17 11:21:19 +1100

    Implement Setup page

--- src/setup/Setup.tsx
+++ src/setup/Setup.tsx
@@ -1,7 +1,64 @@
+import {
+  Button,
+  Container,
+  Group,
+  Paper,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useLocation } from "wouter";
+
+import { updateConnectionString } from "@/lib/client/localstorage";
+import { useGetConfig } from "@/lib/client/query";
+
 export default function Setup() {
+  const [, setLocation] = useLocation();
+  const { data: appConfig } = useGetConfig();
+  const [connectionString, setConnectionString] = useState(
+    appConfig?.connectionString || ""
+  );
+
+  useEffect(() => {
+    if (appConfig != null && appConfig.connectionString) {
+      setConnectionString(appConfig.connectionString);
+    }
+  }, [appConfig]);
+
+  const queryClient = useQueryClient();
+  const connectButtonClicked = () => {
+    updateConnectionString(connectionString);
+    queryClient.setQueryData(["config"], { connectionString });
+    setLocation("/collections");
+  };
+
+  const backButtonClicked = () => {
+    setLocation("/collections");
+  };
+
   return (
-    <>
-      <h1>Setup</h1>
-    </>
+    <Container size={460} my={30}>
+      <Title order={1} ta="center">
+        VectorDB Admin
+      </Title>
+      <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
+        <TextInput
+          label="VectorDB connection string"
+          description="For example ChromaDB http://localhost:8000"
+          placeholder="http://localhost:8000"
+          value={connectionString}
+          onChange={(e) => setConnectionString(e.currentTarget.value)}
+        />
+        <Group mt="lg" justify="flex-end">
+          {appConfig?.connectionString && (
+            <Button variant="default" onClick={backButtonClicked}>
+              Back
+            </Button>
+          )}
+          <Button onClick={connectButtonClicked}>Connect</Button>
+        </Group>
+      </Paper>
+    </Container>
   );
 }
